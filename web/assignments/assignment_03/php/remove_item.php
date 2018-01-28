0444079ef5828de0a84eef2cfebac47c4014ac7e
Grant Sheridan <she08001@byui.edu>
2018-01-27 21:42:58 -0700

    creating view cart

--- web/assignments/assignment_03/php/remove_item.php
+++ web/assignments/assignment_03/php/remove_item.php
@@ -4,9 +4,9 @@
   $item = $_POST['selected'];
 
   if ($item == "rock_one") {
-    $_SESSION['rock_one'] = true;
+    $_SESSION['rock_one'] = false;
   }
   elseif ($item == "rock_two") {
-    $_SESSION['rock_two'] = true;
+    $_SESSION['rock_two'] = false;
   }
 ?>
