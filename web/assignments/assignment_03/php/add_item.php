5558a57e84bd258c7a8f0b818df22156f08b2301
Grant Sheridan <she08001@byui.edu>
2018-01-27 21:16:27 -0700

    debug php session

--- web/assignments/assignment_03/php/add_item.php
+++ web/assignments/assignment_03/php/add_item.php
@@ -1,12 +1,18 @@
 <?php
   $item = $_POST['selected']
-
+  echo "1"
   if ($item == "rock_one") {
+    echo "2a"
     $s = $_SESSION['rock_one'];
+    echo "3a";
     $_SESSION['rock_one'] = !$s;
+    echo "4b";
   }
   elseif ($item == "rock_two") {
+    echo "2b"
     $s = $_SESSION['rock_two'];
+    echo "3b";
     $_SESSION['rock_two'] = !$s;
+    echo "4b";
   }
 ?>
