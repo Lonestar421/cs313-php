2c261dcace6ef98bd0dd0eb2d5cd275390df6e28
Grant Sheridan <she08001@byui.edu>
2018-01-27 21:13:45 -0700

    adding item through php session

--- web/assignments/assignment_03/php/add_item.php
+++ web/assignments/assignment_03/php/add_item.php
@@ -2,9 +2,11 @@
   $item = $_POST['selected']
 
   if ($item == "rock_one") {
-    $_SESSION['rock_one'] = !$_SESSION['rock_one'];
+    $s = $_SESSION['rock_one'];
+    $_SESSION['rock_one'] = !$s;
   }
   elseif ($item == "rock_two") {
-    $_SESSION['rock_two'] = !$_SESSION['rock_two'];
+    $s = $_SESSION['rock_two'];
+    $_SESSION['rock_two'] = !$s;
   }
 ?>
